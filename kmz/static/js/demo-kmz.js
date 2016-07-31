0a309d34b021499bfabe15ec4e77fb1537870527
ThomasG77 <thomas_gratier@yahoo.fr>
2016-07-31 22:56:05 +0200

    Fix MapQuest source issue in KMZ sample

--- kmz/static/js/demo-kmz.js
+++ kmz/static/js/demo-kmz.js
@@ -15,9 +15,7 @@
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.MapQuest({
-              layer: 'sat'
-            })
+            source: new ol.source.OSM()
           }),
           vector
         ],
